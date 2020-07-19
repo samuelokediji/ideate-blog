b8dfaa9f3f00f607b54bd73e238a0d1a827ecf3a
samuelokediji <samuelolaideokediji@gmail.com>
2020-07-19 01:17:06 +0100

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./ID8.png" alt="Gatsby Scene" />
+        <img style={{ margin: 0 }} src="./ID8.png" alt="" />
         <h1>
           Hey people{" "}
           <span role="img" aria-label="wave emoji">
