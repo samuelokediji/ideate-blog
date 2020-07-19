26775e1bf29ecdb32e56159798fbab5c2d151481
samuelokediji <samuelolaideokediji@gmail.com>
2020-07-19 01:14:18 +0100

    ideate blog commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = " Ideate Gatsby Starter Blog"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -15,17 +15,16 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+        <img style={{ margin: 0 }} src="./ID8.png" alt="Gatsby Scene" />
         <h1>
           Hey people{" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>Welcome to Ideate Blog website. .</p>
         <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
+         Come learn about how design meets branding
         </p>
         <p>Now go build something great!</p>
         <Link to="/blog/">
